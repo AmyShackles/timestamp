95814deb360eb7d3ab44513cd638794ca12a2d49
Amy Shackles <amyshackles@gmail.com>
2020-08-27 14:36:57 -0700

    Configured server to return timestamps

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 // where your node app starts
-
+process.env.PORT = 5000;
 // init project
 var express = require('express');
 var app = express();
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,8 +21,25 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
+
+app.get(`/api/timestamp/:date_string`, function (req, res) {
+  const { date_string } = req.params;
+  let date = new Date(date_string);
+  console.log('data string', date_string);
+  console.log('date', date)
+  if (date_string === "") {
+    date = new Date();
+  }
+  const unix = Number(date.getTime());
+  const utc = date.toUTCString();
+  if (utc === "Invalid Date" || unix === null) {
+    res.json({ error: 'Invalid Date' })
+  } else {
+    res.json({ unix: Number(date.getTime()), utc: date.toUTCString() })
+  }
+})
 
 
 
